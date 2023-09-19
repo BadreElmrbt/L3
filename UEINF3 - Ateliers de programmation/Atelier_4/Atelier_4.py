5c4744c39c22d451bec640ea92105f2630f7324f
BadreElmrbt <144235823+BadreElmrbt@users.noreply.github.com>
2023-09-19 14:33:19 +0200

    Ex 6 fini

--- UEINF3 - Ateliers de programmation/Atelier_4/Atelier_4.py
+++ UEINF3 - Ateliers de programmation/Atelier_4/Atelier_4.py
@@ -1,9 +1,8 @@
-from random import *
+from random import randint,shuffle,sample
 import time
 import matplotlib.pyplot as plt
-import numpy as np
 
-# Exercice 1
+#---------------Exercice 1---------------
 def gen_list_random_int(len_max: int = 10, int_binf: int = 0, int_bsup: int = 10) -> list:
     """
     Génère une liste de nombres entiers aléatoires.
@@ -19,7 +18,7 @@ def gen_list_random_int(len_max: int = 10, int_binf: int = 0, int_bsup: int = 10
         count += 1
     return int_nbr
 
-# Exercice 2
+#---------------Exercice 2---------------
 def mix_list(list_to_mix: list) -> list:
     """
     Mélange une liste.
@@ -40,7 +39,7 @@ def mix_list(list_to_mix: list) -> list:
             nbr_alea_list.append(nbr_alea)
     return mixed_list
 
-# Exercice 3
+#---------------Exercice 3---------------
 def choose_element_list(list_in_which_to_choose: list) -> int:
     """
     Choisit un élément aléatoire dans une liste.
@@ -50,7 +49,7 @@ def choose_element_list(list_in_which_to_choose: list) -> int:
     nbr_alea = randint(0, len(list_in_which_to_choose)-1)
     return list_in_which_to_choose[nbr_alea]
 
-# Exercice 4
+#---------------Exercice 4---------------
 def extract_elements_list(list_in_which_to_choose: list, int_nbr_of_element_to_extract: int) -> list:
     """
     Extrait un nombre donné d'éléments aléatoires d'une liste.
@@ -72,32 +71,9 @@ def extract_elements_list(list_in_which_to_choose: list, int_nbr_of_element_to_e
             nbr_alea_list.append(nbr_alea)
     return extracted_list
 
-#----------------Fonction de test----------------
-def test_functions():
-    #Exercice 1
-    list1 = gen_list_random_int()
-    print("Exercice 1 - Liste générée aléatoirement : ", list1)
-
-    # Exercice 2
-    list2 = mix_list(list1)
-    print("Exercice 2 - Liste mélangée : ", list2)
-
-    # Exercice 3
-    element = choose_element_list(list1)
-    print("Exercice 3 - Élément choisi : ", element)
-
-    # Exercice 4
-    num_to_extract = 3 
-    extracted_list = extract_elements_list(list1, num_to_extract)
-    print("Exercice 4 - ", num_to_extract, "éléments extraits : ", extracted_list)
-
-test_functions()
-
-#Exercice 5
+#---------------Exercice 5---------------
 #Question 1
-listNbr = [10, 500, 5000, 50000, 100000]
-
-def perf_list(mix_list, shuffle, listInt, n)->callable:
+def perf_list(mix_list, shuffle, listInt, n):
     time_shuffle = []
     time_mix = []
     for i in listInt:
@@ -115,66 +91,110 @@ def perf_list(mix_list, shuffle, listInt, n)->callable:
             times_mix.append(end_pc - start_pc)
         time_shuffle.append(sum(times_shuffle) / n)
         time_mix.append(sum(times_mix) / n)
-    return (time_shuffle, time_mix)
+    return (time_mix, time_shuffle)
 
-result = perf_list(mix_list, shuffle, [5,10,20], 10)
-
+#Question 2
 def perf_extract(sample, extract_elements_list, listInt, n, int_nbr_of_element_to_extract):
     time_extract = []
     time_sample = []
     for i in listInt:
         times_extract = []
         times_sample = []
+        liste = gen_list_random_int(i)
         for _ in range(n):
-            liste = [gen_list_random_int(i)]
-
             start_pc = time.perf_counter()
             extract_elements_list(liste,int_nbr_of_element_to_extract)
             end_pc = time.perf_counter()
             times_extract.append(end_pc - start_pc)
-
             start_pc = time.perf_counter() 
             sample(liste,int_nbr_of_element_to_extract)
             end_pc = time.perf_counter()
             times_sample.append(end_pc - start_pc)
-
         time_extract.append(sum(times_extract) / n)
         time_sample.append(sum(times_sample) / n)
-
     return (time_extract, time_sample)
 
-resultDeux = perf_extract(sample, extract_elements_list,listNbr, 100, 5 )
-
-# --------------Affichage---------------
-x_axis_list = [5,10,20]
-fig, ax = plt.subplots()
-
-ax.plot(x_axis_list,result[0],'bo-',label='Shuffle') 
-ax.plot(x_axis_list,result[1], 'r*-', label='Mix') 
-ax.set(xlabel='Taille de la liste', ylabel='Temps (s)', title="Comparaison temps fonction") 
-ax.legend(loc='upper center', shadow=True, fontsize='x-large') 
-plt.show()
-
-x_axis_list = listNbr
-fig, ax = plt.subplots()
-ax.plot(x_axis_list,resultDeux[0],'bo-',label='extract') 
-ax.plot(x_axis_list,resultDeux[1], 'r*-', label='sample') 
-ax.set(xlabel='Taille de la liste', ylabel='Temps (s)', title="Comparaison temps fonction") 
-ax.legend(loc='upper center', shadow=True, fontsize='x-large') 
-plt.show()
-
-#----------------------Exercice 6----------------------
+#---------------Exercice 6---------------
 #Question 1
-
-def sort_list(liste):
+def sort_list(liste1):
+    liste = list(liste1)
     for i in range(len(liste)):
         min_index = i
         for j in range(i+1, len(liste)):
             if liste[j] < liste[min_index]:
                 min_index = j
         liste[i], liste[min_index] = liste[min_index], liste[i]
-
     return liste
 
-l1 = [9,5,6,-5,12,5000]
-print(sort_list(l1))
+#Question 2
+def perf_list2(sorted, sort_list, listInt, n):
+    time_sort_list = []
+    time_mix = []
+    for i in listInt:
+        times_sort_list = []
+        times_mix = []
+        for _ in range(n):
+            liste = gen_list_random_int(i)
+            start_pc = time.perf_counter()
+            sort_list(liste)
+            end_pc = time.perf_counter()
+            times_sort_list.append(end_pc - start_pc)
+            start_pc = time.perf_counter() 
+            sorted(liste)
+            end_pc = time.perf_counter()
+            times_mix.append(end_pc - start_pc)
+        time_sort_list.append(sum(times_sort_list) / n)
+        time_mix.append(sum(times_mix) / n)
+    return (time_sort_list, time_mix)
+
+#----------------Fonction de test----------------
+def test_functions():
+    #Exercice 1
+    list1 = gen_list_random_int()
+    print("Exercice 1 - Liste générée aléatoirement : ", list1)
+
+    # Exercice 2
+    list2 = mix_list(list1)
+    print("Exercice 2 - Liste mélangée : ", list2)
+
+    # Exercice 3
+    element = choose_element_list(list1)
+    print("Exercice 3 - Élément choisi : ", element)
+
+    # Exercice 4
+    num_to_extract = 3 
+    extracted_list = extract_elements_list(list1, num_to_extract)
+    print("Exercice 4 -", num_to_extract, "éléments extraits : ", extracted_list)
+
+    #Exercice 5
+    result = perf_list(mix_list, shuffle, [5,10,20], 10)
+    x_axis_list = [5,10,20]
+    fig, ax = plt.subplots()
+    ax.plot(x_axis_list,result[0], 'bo-', label='Mix_list (ma fonction)') 
+    ax.plot(x_axis_list,result[1],'r*-',label='Shuffle (fonction python)') 
+    ax.set(xlabel='Taille de la liste', ylabel='Temps (s)', title="Comparaison temps fonction") 
+    ax.legend(loc='upper center', shadow=True, fontsize='x-large') 
+
+    listNbr = [10, 500, 5000, 50000, 100000]
+    resultDeux = perf_extract(sample, extract_elements_list,listNbr, 100, 5)
+    x_axis_list = listNbr
+    fig, ax = plt.subplots()
+    ax.plot(x_axis_list,resultDeux[0],'bo-',label='Extract (ma fonction)') 
+    ax.plot(x_axis_list,resultDeux[1], 'r*-', label='Sample (fonction python)')
+    ax.set(xlabel='Taille de la liste', ylabel='Temps (s)', title="Comparaison temps fonction") 
+    ax.legend(loc='upper center', shadow=True, fontsize='x-large') 
+
+    # Exercice 6
+    sorted_list = sort_list(list1)
+    print("Exercice 6 - liste triée : ", sorted_list)
+
+    result = perf_list2(sorted, sort_list, [5,10,20], 10)
+    x_axis_list = [5,10,20]
+    fig, ax = plt.subplots()
+    ax.plot(x_axis_list,result[0],'bo-',label='Sort_list (ma fonction)') 
+    ax.plot(x_axis_list,result[1], 'r*-', label='Sorted (fonction python)') 
+    ax.set(xlabel='Taille de la liste', ylabel='Temps (s)', title="Comparaison temps fonction") 
+    ax.legend(loc='upper center', shadow=True, fontsize='x-large') 
+    plt.show()
+
+test_functions()
